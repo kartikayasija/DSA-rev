a95cfbfdb8b5ea1f98a221ad39e81db781f164aa
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-01 14:39:08 +0530

    Time: 0 ms (100%), Space: 7 MB (8.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         vector<string> map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
         // to handle the example 2
         if(digits.size()==0){
             return {};
@@ -18,12 +19,14 @@ public:
 
         //rec
         vector<string> rec_ans = f(digits,idx+1,map);
-        string unit_string=map[digits[idx]-'0'];
+
+
+        string unit_string= map[digits[idx] - '0'];
         vector<string> my_ans;
-        for(auto x: rec_ans){
-            for(auto s: unit_string){
-                string resultant = s+x;
-                my_ans.push_back(resultant);
+
+        for(auto x: rec_ans){  
+            for(auto s: unit_string){ //abc
+                my_ans.push_back(s+x);
             }
         }      
         return my_ans;
@@ -34,8 +37,21 @@ public:
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 
 Expectation ->
-"234" -> ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+"234" -> ["adg","bdg","cdg","aeg","beg","ceg","afg","bfg","cfg","adh","bdh","cdh","aeh","beh","ceh","afh","bfh","cfh","adi","bdi","cdi","aei","bei","cei","afi","bfi","cfi"]
 
-tru
+2   3   4
 
+a   d   g
+b   e   h
+c   f   i
+
+faith ->
+"34" -> [ye recursion nikalega]
+
+our work ->
+"234" -> "2" left ('abc') 
+recursion ke har ans ke age 'a' then 'b' then 'c'
+
+"2" -> 2 -> map[2] -> "abc"
+ 
 */
