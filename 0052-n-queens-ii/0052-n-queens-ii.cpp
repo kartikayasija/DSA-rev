8b72303af63c1fa061c99c53e510a89e8e5617d2
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-06 17:56:54 +0530

    Time: 6 ms (43.82%), Space: 6.5 MB (48.62%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -2,37 +2,44 @@ class Solution {
 public:
     int ways = 0;
     int totalNQueens(int n) {
-        vector<vector<bool>> chess(n, vector<bool>(n, false));
-        f(n, 0, chess);
+        vector<bool> temp2(n, false);
+        vector<vector <bool>> temp(n, temp2);
+
+        f(n, 0, temp);
         return ways;
     }
 
-    void f(int n, int idx, vector<vector<bool>> &chess){
-        
-        if(idx==n){
+    void f(int n, int idx, vector<vector <bool>> &temp){
+
+        //base
+        if(idx == n){
             ways++;
             return;
         }
 
+
         //rec
-        for(int i=0; i<n; i++){
-            if(!isSafe(idx, i, chess)){
-                continue;
+        for(int col=0; col<n; col++){
+
+            if(isSafe(temp, idx, col)){
+                
+                temp[idx][col] = true;
+
+                f(n, idx+1, temp);
+
+                //backtrack
+                temp[idx][col] = false;
+
             }
 
-            chess[idx][i] = true;
-            f(n, idx+1, chess);
-
-            //backtrack
-            chess[idx][i] = false;
         }
 
     }
-    bool isSafe(int row, int col, vector<vector<bool>> &chess){
 
+    bool isSafe(vector<vector <bool>> &temp, int row, int col){
         //verticle
-        for(int i=0; i<row; i++){
-            if(chess[i][col]){
+        for(int i=row; i>=0; i--){
+            if(temp[i][col]){
                 return false;
             }
         }
@@ -40,18 +47,17 @@ public:
         //left diag
         int i = row;
         int j = col;
-
         while(i>=0 and j>=0){
-            if(chess[i--][j--]){
+            if(temp[i--][j--]){
                 return false;
             }
-        } 
+        }
 
         //right diag
         i = row;
         j = col;
-        while(i>=0 and j<chess.size()){
-            if(chess[i--][j++]){
+        while(i>=0 and j<temp.size()){
+            if(temp[i--][j++]){
                 return false;
             }
         }
