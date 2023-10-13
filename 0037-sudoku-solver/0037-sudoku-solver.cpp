9d07dc31dc2da65ed73f6028307ec05a935fcb51
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-14 02:29:19 +0530

    Time: 32 ms (12.93%), Space: 6.9 MB (20.94%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,93 +1,86 @@
 class Solution {
 public:
-vector<vector<char>>result;
+    vector<vector<char>> result;
+
+    //main
     void solveSudoku(vector<vector<char>>& board) {
         f(board, 0, 0);
         board = result;
     }
 
-    void f(vector<vector<char>> &board, int row, int col){
+    //rec
+    void f(vector<vector<char>>& board, int row, int col){
 
         //base
-        if(row==board.size()){
+        if(row == board.size()){
             result = board;
             return;
-        }
-
-        
-        // reinitialise col, if it is at end and increase row
-        int nextRow, nextCol;
-
-        if(col == board[0].size()-1){
-            nextRow = row+1;
-            nextCol = 0;
-        } else{
-            nextRow = row;
-            nextCol = col+1;
         }
         
 
         //rec
-        if(board[row][col]!='.'){
+        int nextRow, nextCol;
+        if(col == board[0].size()){
+            nextRow = row+1;
+            nextCol = 0;
+        } else {
+            nextRow = row;
+            nextCol = col+1;
+        }
+        
+        if(board[row][col] != '.'){
             f(board, nextRow, nextCol);
-        } else{
-            for(char i='1'; i<='9'; i++){
-                if(!isValid(board, row, col, i)){
-                    continue;
-                }
+            return;
+        }
 
-                board[row][col] = i;
-
-                f(board, nextRow, nextCol);
-
-                //backtrack
-                board[row][col] = '.';
+        for(char i='1'; i<='9'; i++){
+            // i will be converted to int
+            if(!isValid(board, row, col, i)){
+                continue;
             }
+
+            board[row][col] = i;
+            f(board, nextRow, nextCol);
+
+            board[row][col] = '.';
         }
     }
 
-    bool isValid(vector<vector<char>>& board, int row, int col, char number){
-
+    //helpers
+    bool isValid(vector<vector<char>>& board, int row, int col, int target){
         //row
         for(int i=0; i<board[0].size(); i++){
-            if(board[row][i]==number){
+            if(board[row][i] == target){
                 return false;
             }
         }
 
         //col
         for(int i=0; i<board.size(); i++){
-            if(board[i][col]==number){
+            if(board[i][col] == target){
                 return false;
             }
         }
 
+        int startRow = getMatrix(row);
+        int startCol = getMatrix(col);
         //in matrix
-        int matrixRowStart = matrixD(row);
-        int matrixColStart = matrixD(col);
-
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
-                if(board[matrixRowStart+i][matrixColStart+j] == number){
+                if(board[startRow+i][startCol+j] == target){
                     return false;
                 }
             }
         }
-        
 
         return true;
     }
 
-
-    /*
-        returns starting row/col of the number
-        from which the number belongs
-    */
-
-    int matrixD(int number){
-        while(number%3!=0){
-            number--;
+    int getMatrix(int i){
+        while(i%3!=0){
+            i--;
         }
-        return number;
+
+        return i;
     }
 };
