75ddde35ff539d0722f01308edcb3d2e7d019011
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-14 14:22:31 +0530

    Time: 4 ms (59.15%), Space: 12.1 MB (19.71%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<vector<int>> result;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> temp;
         sort(candidates.begin(), candidates.end());
+        f(candidates, {}, target, 0);
 
-        f(candidates, temp, target, 0);
         return result;
     }
 
-    void f(vector<int>& candidates, vector<int> &temp, int target, int idx){
+    void f(vector<int>& candidates, vector<int> temp, int target, int idx){
         //base
         if(target==0){
             result.push_back(temp);
@@ -17,8 +16,10 @@ public:
         }
 
         //rec
-        for(int i=idx; i<candidates.size();i++){
-            if (i > idx && candidates[i] == candidates[i - 1]){
+        for(int i=idx; i<candidates.size(); i++){
+            // when i>idx, means it is not the firs6 run of loop
+            // i is incremented
+            if(i>idx and candidates[i] == candidates[i-1]){
                 continue;
             }
             if(target<candidates[i]){
@@ -28,13 +29,9 @@ public:
             temp.push_back(candidates[i]);
 
             f(candidates, temp, target - candidates[i], i+1);
-            /*
-                combination sum 1, 2 me inka index, for loop ke kaam hi aega
-            */
 
             //backtrack
             temp.pop_back();
         }
     }
-    
-}; 
+};
