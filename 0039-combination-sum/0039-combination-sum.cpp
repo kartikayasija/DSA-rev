3e68cfe94a6cbdfa03e3700469e51f7ca1487fe7
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-14 14:02:15 +0530

    Time: 4 ms (74.4%), Space: 11.3 MB (49.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,30 +2,37 @@ class Solution {
 public:
     vector<vector<int>> result;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        f(candidates, {}, target, 0, 0);
-
+        vector<int> temp;
+        f(candidates, temp, target, 0);
         return result;
     }
 
-    void f(vector<int>& candidates, vector<int> temp, int target, int sum, int idx){
-        if(sum == target){
+    void f(vector<int>& candidates, vector<int>& temp, int target, int idx){
+        //base
+        if(target==0){
             result.push_back(temp);
             return;
         }
 
         //rec
         for(int i=idx; i<candidates.size(); i++){
-            if(candidates[i]+sum > target){
+            if(target<candidates[i]){
                 continue;
             }
 
-            sum = sum+candidates[i];
             temp.push_back(candidates[i]);
-            f(candidates, temp, target, sum, i);
 
+            f(candidates, temp, target - candidates[i], i);
+            /*
+                idx ko i isly bheja kykuki
+                [2,3,5] ->
+                [2] -> 2,3,5   --- i was 0 and had to continue from 0 only
+                [3] -> X,3,5       i was 1 and had to continue from 1 idx only
+                [5] -. X,X,5       i was 2 and had to continue from 2 idx only ,
+            */
             //backtrack
             temp.pop_back();
-            sum = sum-candidates[i];
         }
     }
+
 };
