0226f7b17121a5d8ddce6970ce0278c21436f853
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-14 14:47:19 +0530

    Time: 2 ms (62.32%), Space: 8.4 MB (19.4%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<vector<int>> result;
     vector<vector<int>> permute(vector<int>& nums) {
-        /*
-            initialise it with -11 because -10 <= nums[i] <=10
-        */ 
+        vector<int> temp(nums.size(), -11);
 
-        vector<int> temp(nums.size(), -11); 
-        f(nums, 0, temp);
+        f(nums, temp, 0);
+
         return result;
     }
 
-    void f(vector<int>& nums, int idx, vector<int> &temp){
+    void f(vector<int>& nums, vector<int>& temp, int idx){
 
         //base
-        if(idx==nums.size()){
+        if(idx == nums.size()){
             result.push_back(temp);
             return;
         }
 
 
         //rec
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<temp.size(); i++){
             if(temp[i] != -11){
                 continue;
             }
-            temp[i] = nums[idx]; 
-            f(nums, idx+1, temp);
+
+            temp[i] = nums[idx];
+            f(nums, temp, idx+1);
+            
             temp[i] = -11;
         }
     }
