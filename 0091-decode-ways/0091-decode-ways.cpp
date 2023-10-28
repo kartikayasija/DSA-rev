a74dc5a32b912a4cc1f4be2f52cc36bceb7f1e46
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-10-29 00:49:56 +0530

    Time: 4 ms (21.57%), Space: 6.8 MB (32.2%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        if (n == 0 || s[0] == '0') {
-            return 0; // If the string is empty or starts with '0', there are no valid decodings.
+        if (s.size() == 0 || s[0] == '0') {
+            return 0;
         }
-
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(s.size(), 0);
         dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            if (s[i - 1] != '0') {
-                dp[i] = dp[i - 1];
+        for(int i=1; i<dp.size(); i++){
+            if(s[i-1]=='0' and s[i]=='0'){
+                dp[i]=0;
             }
-
-            int twoDigit = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                dp[i] += dp[i - 2];
+            else if(s[i-1]=='0' and s[i]!='0'){
+                dp[i] = (i>=2) ? dp[i-1] : 0;
+            }
+            else if(s[i-1]!='0' and s[i]=='0'){
+                int n = makeTodigit(s[i-1], s[i]);
+                if(n<=26){
+                    dp[i] = (i>=2) ? dp[i-2] : 1;
+                }
+            }
+            else if(s[i-1]!='0' and s[i]!='0'){
+                dp[i]=dp[i-1];
+                int n = makeTodigit(s[i-1], s[i]);
+                if(n<=26){
+                    dp[i] += (i>=2) ? dp[i-2] : 1;
+                }
             }
         }
 
-        return dp[n];
+        return dp[s.size()-1];
+    }
+
+    int makeTodigit(char a, char b){
+        string s ="";
+        s = s+a+b;
+        return stoi(s);
     }
 };
