358e043979de76be167fa59253af15e135951d34
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-11-05 02:51:19 +0530

    Time: 12 ms (12.33%), Space: 10.4 MB (26.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int> (grid[0].size(), 0));
+        vector<vector<int>> memo(grid.size(), vector<int> (grid[0].size(), -1));
+        return f(grid, 0, 0, memo);
+    }
 
+    int f(vector<vector<int>>& grid, int row, int col, vector<vector<int>> &memo) {
 
-        for(int i=dp.size()-1; i>=0; i--){
-            for(int j = dp[0].size() - 1; j>=0; j--){
-
-                if(i==dp.size()-1 and j==dp[0].size()-1){
-                    dp[i][j] = grid[i][j];
-                }
-
-                //last row
-                else if(i==dp.size()-1){  
-                    dp[i][j] = dp[i][j+1] + grid[i][j];
-                } 
-                
-                //rightest col
-                else if(j==dp[0].size()-1){
-                    dp[i][j] = dp[i+1][j] + grid[i][j];
-                }
-
-                // else everyone will have 2 choices
-                else{
-                    dp[i][j] = min(dp[i][j+1], dp[i+1][j]) + grid[i][j];
-                }
-            }
+        //base
+        if(row == grid.size() or col == grid[0].size()){
+            return INT_MAX;
+        }
+        if(row == grid.size()-1 and col == grid[0].size()-1){
+            return grid[row][col];
         }
 
-        return dp[0][0];
+        //memo
+        if(memo[row][col] != -1){
+            return memo[row][col];
+        }
+        //rec
+        int sum1 = f(grid, row+1, col, memo);
+        int sum2 = f(grid, row, col+1, memo);
+        
+        return memo[row][col] = min(sum1, sum2) + grid[row][col];
     }
 };
