23776aa4f146c967ab107e6d9acb97518e34dcb5
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-11-09 00:59:49 +0530

    Time: 11 ms (74.97%), Space: 13.1 MB (35.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
+    int totalSum;
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums, 0, target);
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if((totalSum+target)%2 != 0) {
+            return 0;
+        }
+        int newTarget = (totalSum + target) / 2;
+        if(newTarget <0 ){
+            return 0;
+        }
+        vector<vector<int>> memo(nums.size()+1, vector<int> (newTarget+1, -1));
+        return f(nums, 0, newTarget, memo);
     }
 
-    int f(vector<int>& nums, int idx, int target) {
+    int f(vector<int>& nums, int idx, int target, vector<vector<int>>& memo) {
 
         //base
         if (idx == nums.size()) {
             if (target == 0) {
                 return 1;
             }
-
             return 0;
         }
 
+        //memo
+        if(memo[idx][target] != -1) {
+            return memo[idx][target];
+        }
 
         //rec
-        int a1 = f(nums, idx+1, target-nums[idx]);
-        int a2 = f(nums, idx+1, target+nums[idx]);
 
-        return a1+a2;
+        int considered = (target >= nums[idx]) ? f(nums, idx+1, target-nums[idx], memo) : 0;
+        int notConsidered = f(nums, idx+1, target, memo);
+
+        return memo[idx][target] = considered+notConsidered;
     }
 };
