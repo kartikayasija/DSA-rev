686494c49f6d948b3cc2471624c0e214de5b4cf8
Kartikay Asija <115306535+kartikayasija@users.noreply.github.com>
2023-11-09 02:08:45 +0530

    Time: 26 ms (46.3%), Space: 19.4 MB (16.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount+1, 0);
-        dp[0] = 1;
+        vector<vector<int>> memo(coins.size()+1, vector<int> (amount+1, -1));
+        return f(coins, amount, 0, memo);
+    }
 
-        for(int coin: coins){
-            for(int i=coin; i<dp.size(); i++){
-                dp[i] += dp[i - coin];
-            }
+    int f(vector<int>& coins, int amount, int idx, vector<vector<int>>& memo) {
+        //base
+        if(amount == 0) {
+            return 1;
         }
-        return dp[amount];
+        if(idx == coins.size()){
+            return 0;
+        }
+
+        //memo
+        if(memo[idx][amount] != -1) {
+            return memo[idx][amount];
+        }
+
+        //rec
+        int considered = (amount>=coins[idx]) ? f(coins, amount - coins[idx], idx, memo) : 0;
+        int notConsidered = f(coins, amount, idx+1, memo);
+
+        return memo[idx][amount] = considered + notConsidered;
     }
 };
 
